eea53ddd3a6d7f8492e1a8733422291120a0bc03
Akram Abdeslem Chaima <aslackw@riseup.net>
2022-11-27 19:53:31 +0100

    update

---
+++ x86_64/update_repo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm sonegx_repo*
+
+echo "repo-add"
+repo-add -s -n -R sonegx_repo.db.tar.gz *.pkg.tar.zst
+
+sleep 1
+
+rm sonegx_repo.db
+rm sonegx_repo.db.sig
+
+rm sonegx_repo.files
+rm sonegx_repo.files.sig
+
+mv sonegx_repo.db.tar.gz sonegx_repo.db
+mv sonegx_repo.db.tar.gz.sig sonegx_repo.db.sig
+
+mv sonegx_repo.files.tar.gz sonegx_repo.files
+mv sonegx_repo.files.tar.gz.sig sonegx_repo.files.sig
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
